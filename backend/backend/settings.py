01ea5f2cf60db8972716c91b52c77bedf139b35b
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2024-12-04 12:24:23 +0300

    database setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from pymongo import MongoClient # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'mongo_api'
 ]
 
 MIDDLEWARE = [
@@ -79,6 +82,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db = MongoClient('localhost', 27017).gridTrial
 
 
 # Password validation
--- backend/mongo_api/views.py
+++ backend/mongo_api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from pymongo import MongoClient # type: ignore
+from backend.settings import db
+from gridfs import GridFS # type: ignore 
 
 # Create your views here.
+fs = GridFS(db)
